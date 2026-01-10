5a2b8f390bb406c04128a181ad669b304d5c70e8
Deathemonic <zinth.chan@tuta.io>
2026-01-10 20:40:32 +0800

    fix: system types

--- Assembly/TypeStringConverter.cs
+++ Assembly/TypeStringConverter.cs
@@ -1,12 +1,12 @@
+using System.Collections.Frozen;
 using dnlib.DotNet;
-using MemoryPackDumper.Helpers;
 using ZLinq;
 
 namespace MemoryPackDumper.Assembly;
 
 public static class TypeStringConverter
 {
-    private static readonly Dictionary<string, string> TypeMap = new()
+    private static readonly FrozenDictionary<string, string> TypeMap = new Dictionary<string, string>
     {
         ["System.String"] = "string",
         ["System.Int16"] = "short",
@@ -20,46 +20,30 @@ public static class TypeStringConverter
         ["System.Double"] = "double",
         ["System.SByte"] = "sbyte",
         ["System.Byte"] = "byte",
-        ["System.Decimal"] = "decimal"
-    };
+        ["System.Decimal"] = "decimal",
+        ["System.Object"] = "object",
+        ["System.Void"] = "void",
+        ["System.Char"] = "char",
+        ["System.IntPtr"] = "nint",
+        ["System.UIntPtr"] = "nuint"
+    }.ToFrozenDictionary();
 
     public static string TypeToString(TypeSig typeSig)
     {
         if (typeSig == null) return "void";
 
-        if (typeSig is GenericInstSig genericInstance) return ConvertGenericType(genericInstance);
-
-        if (typeSig is SZArraySig szArray)
+        return typeSig switch
         {
-            return TypeToString(szArray.Next) + "[]";
-        }
-        
-        if (typeSig is ArraySig array)
-        {
-            return TypeToString(array.Next) + "[]";
-        }
-
-        var typeDef = typeSig.TryGetTypeDef();
-        return typeDef != null ? SystemToStringType(typeDef) : CheckSystemType(typeSig);
-    }
-
-    private static string CheckSystemType(TypeSig typeSig)
-    {
-        if (TypeMap.TryGetValue(typeSig.FullName, out var type))
-            return type;
-
-        var name = typeSig.TypeName;
-        var ns = typeSig.Namespace ?? "";
-        if (ns.StartsWith("System"))
-             Log.Global.LogUnknownSystemType(name);
-
-        return name;
+            GenericInstSig genericInstance => ConvertGenericType(genericInstance),
+            SZArraySig szArray => TypeToString(szArray.Next) + "[]",
+            ArraySig array => TypeToString(array.Next) + "[]",
+            _ => TypeMap.GetValueOrDefault(typeSig.FullName, typeSig.TypeName)
+        };
     }
 
     private static string ConvertGenericType(GenericInstSig genericInstance)
     {
         var baseType = genericInstance.GenericType.TypeName;
-
         if (baseType.Contains('`')) baseType = baseType[..baseType.IndexOf('`')];
 
         var genericArgs = genericInstance.GenericArguments.AsValueEnumerable().Select(TypeToString).JoinToString(", ");
@@ -68,16 +52,6 @@ public static class TypeStringConverter
 
     public static string SystemToStringType(TypeDef typeDef)
     {
-        var fullName = typeDef.FullName;
-        if (TypeMap.TryGetValue(fullName, out var type))
-            return type;
-
-        var name = typeDef.Name.String;
-        var ns = typeDef.Namespace?.String ?? "";
-
-        if (ns.StartsWith("System"))
-            Log.Global.LogUnknownSystemType(name);
-
-        return name;
+        return TypeMap.GetValueOrDefault(typeDef.FullName, typeDef.Name.String);
     }
 }
